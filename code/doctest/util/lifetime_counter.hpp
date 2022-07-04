c9a92e43f0c0c7956aa5b2b46b8b3b16e3a9d612
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-07-04 16:42:45 +0300

    Basic lifetime counting

--- code/doctest/util/lifetime_counter.hpp
+++ code/doctest/util/lifetime_counter.hpp
@@ -13,43 +13,103 @@ namespace doctest
 namespace util
 {
 
+namespace impl
+{
+struct lifetime_stats
+{
+    int d_ctr = 0; // default construct
+    int c_ctr = 0; // copy constructed
+    int c_asgn = 0; // copy assigned
+    int copies = 0; // total copies
+    int m_ctr = 0; // move constructed
+    int m_asgn = 0; // move assigned
+    int living = 0; // total living
+
+    lifetime_stats* parent = nullptr;
+
+    template <typename F>
+    void for_all(F&& f)
+    {
+        for (auto s = this; s; s = parent)
+        {
+            f(*s);
+        }
+    }
+};
+}
+
 template <typename Tag>
 class lifetime_counter
 {
 public:
-    struct stats
+    struct stats : public impl::lifetime_stats
     {
-        int d_ctr = 0; // default construct
-        int copies = 0; // total copies
-        int c_ctr = 0; // copy constructed
-        int c_asgn = 0; // copy assigned
-        int m_ctr = 0; // move constructed
-        int m_asgn = 0; // move assigned
-        int living = 0; // total living
-
-        stats* parent = nullptr;
+        stats()
+        {
+            parent = lifetime_counter::top;
+            lifetime_counter::top = this;
+        }
 
         ~stats()
         {
+            lifetime_counter::top = parent;
         }
     };
 
     static stats* top;
     static stats bottom;
 
-    lifetime_counter() {
+    lifetime_counter()
+    {
+        top->for_all([](impl::lifetime_stats& s) {
+            ++s.d_ctr;
+            ++s.living;
+        });
     }
-    lifetime_counter(const lifetime_counter& other) {
+
+    lifetime_counter(const lifetime_counter&)
+    {
+        top->for_all([](impl::lifetime_stats& s) {
+            ++s.c_ctr;
+            ++s.copies;
+            ++s.living;
+        });
     }
-    lifetime_counter& operator=(const lifetime_counter& other) {
+
+    lifetime_counter& operator=(const lifetime_counter&)
+    {
+        top->for_all([](impl::lifetime_stats& s) {
+            ++s.c_asgn;
+            ++s.copies;
+        });
     }
-    lifetime_counter(lifetime_counter&& other) {
+
+    lifetime_counter(lifetime_counter&&)
+    {
+        top->for_all([](impl::lifetime_stats& s) {
+            ++s.m_ctr;
+            ++s.living;
+        });
     }
-    lifetime_counter& operator=(lifetime_counter&& other) {
+
+    lifetime_counter& operator=(lifetime_counter&&)
+    {
+        top->for_all([](impl::lifetime_stats& s) {
+            ++s.m_asgn;
+        });
     }
-    ~lifetime_counter() {
+    ~lifetime_counter()
+    {
+        top->for_all([](impl::lifetime_stats& s) {
+            --s.living;
+        });
     }
 };
+
+template <typename T>
+typename lifetime_counter<T>::stats* lifetime_counter<T>::top;
+template <typename T>
+typename lifetime_counter<T>::stats lifetime_counter<T>::bottom;
 
 }
 }
--- scratch/scratch.cpp
+++ scratch/scratch.cpp
@@ -1,3 +1,11 @@
+// doctest-util
+// Copyright(c) 2022 Borislav Stanimirov
+//
+// SPDX-License-Identifier: MIT
+// Distributed under the MIT Software License
+// See accompanying file LICENSE.txt or https://opensource.org/licenses/MIT
+//
+
 #include <doctest/util/lifetime_counter.hpp>
 #include <iostream>
 
@@ -6,4 +14,6 @@ int main()
     using doctest::util::lifetime_counter;
 
     lifetime_counter<int> lc;
+
+    auto lc2 = lc;
 }
