48436b8164167ca11a37dcf5a1d3446a30a32c93
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-08-31 08:17:34 +0300

    Lifetime counter

--- code/doctest/util/lifetime_counter.hpp
+++ code/doctest/util/lifetime_counter.hpp
@@ -15,8 +15,9 @@ namespace util
 
 namespace impl
 {
-struct lifetime_stats
+class lifetime_stats
 {
+public:
     int d_ctr = 0; // default construct
     int c_ctr = 0; // copy constructed
     int c_asgn = 0; // copy assigned
@@ -25,16 +26,27 @@ struct lifetime_stats
     int m_asgn = 0; // move assigned
     int living = 0; // total living
 
-    lifetime_stats* parent = nullptr;
-
     template <typename F>
     void for_all(F&& f)
     {
-        for (auto s = this; s; s = parent)
+        for (auto s = this; s; s = s->parent)
         {
             f(*s);
         }
     }
+
+    bool operator==(const lifetime_stats& other) const
+    {
+        return d_ctr == other.d_ctr
+            && c_ctr == other.c_ctr
+            && c_asgn == other.c_asgn
+            && copies == other.copies
+            && m_ctr == other.m_ctr
+            && m_ctr == other.m_asgn
+            && living == other.living;
+    }
+protected:
+    lifetime_stats* parent = nullptr;
 };
 }
 
@@ -42,26 +54,26 @@ template <typename Tag>
 class lifetime_counter
 {
 public:
-    struct stats : public impl::lifetime_stats
+    struct lifetime_stats : public impl::lifetime_stats
     {
-        stats()
+        lifetime_stats()
         {
-            parent = lifetime_counter::top;
-            lifetime_counter::top = this;
+            parent = lifetime_counter::m_top_stats;
+            lifetime_counter::m_top_stats = this;
         }
 
-        ~stats()
+        ~lifetime_stats()
         {
-            lifetime_counter::top = parent;
+            lifetime_counter::m_top_stats = parent;
         }
     };
 
-    static stats* top;
-    static stats bottom;
+    static const lifetime_stats& root_lifetime_stats() { return m_root_stats; }
+    static const impl::lifetime_stats& top_lifetime_stats() { return *m_top_stats; }
 
     lifetime_counter()
     {
-        top->for_all([](impl::lifetime_stats& s) {
+        m_top_stats->for_all([](impl::lifetime_stats& s) {
             ++s.d_ctr;
             ++s.living;
         });
@@ -69,7 +81,7 @@ public:
 
     lifetime_counter(const lifetime_counter&)
     {
-        top->for_all([](impl::lifetime_stats& s) {
+        m_top_stats->for_all([](impl::lifetime_stats& s) {
             ++s.c_ctr;
             ++s.copies;
             ++s.living;
@@ -78,15 +90,16 @@ public:
 
     lifetime_counter& operator=(const lifetime_counter&)
     {
-        top->for_all([](impl::lifetime_stats& s) {
+        m_top_stats->for_all([](impl::lifetime_stats& s) {
             ++s.c_asgn;
             ++s.copies;
         });
+        return *this;
     }
 
     lifetime_counter(lifetime_counter&&)
     {
-        top->for_all([](impl::lifetime_stats& s) {
+        m_top_stats->for_all([](impl::lifetime_stats& s) {
             ++s.m_ctr;
             ++s.living;
         });
@@ -94,22 +107,33 @@ public:
 
     lifetime_counter& operator=(lifetime_counter&&)
     {
-        top->for_all([](impl::lifetime_stats& s) {
+        m_top_stats->for_all([](impl::lifetime_stats& s) {
             ++s.m_asgn;
         });
+        return *this;
     }
     ~lifetime_counter()
     {
-        top->for_all([](impl::lifetime_stats& s) {
+        m_top_stats->for_all([](impl::lifetime_stats& s) {
             --s.living;
         });
     }
+
+private:
+    static impl::lifetime_stats* m_top_stats;
+    static lifetime_stats m_root_stats;
 };
 
 template <typename T>
-typename lifetime_counter<T>::stats* lifetime_counter<T>::top;
+typename impl::lifetime_stats* lifetime_counter<T>::m_top_stats;
 template <typename T>
-typename lifetime_counter<T>::stats lifetime_counter<T>::bottom;
+typename lifetime_counter<T>::lifetime_stats lifetime_counter<T>::m_root_stats;
+
+struct lifetime_counter_sentry {
+    const impl::lifetime_stats& stats;
+    lifetime_counter_sentry(const impl::lifetime_stats& s) : stats(s) {}
+    ~lifetime_counter_sentry() { CHECK(stats.living == 0); }
+};
 
 }
 }
--- test/t-lifetime_counter.cpp
+++ test/t-lifetime_counter.cpp
@@ -1,6 +1,66 @@
 #include <doctest/util/lifetime_counter.hpp>
 
-TEST_CASE("lifetime counter")
+TEST_CASE("bare lifetime counter")
 {
+    using lc = doctest::util::lifetime_counter<void>;
+    using lci = doctest::util::lifetime_counter<int>;
 
+    auto& lc_root = lc::root_lifetime_stats();
+    auto& lci_root = lci::root_lifetime_stats();
+
+    doctest::util::lifetime_counter_sentry sentry_lc(lc_root), sentry_lci(lci_root);
+
+    CHECK(&lc_root == &lc::top_lifetime_stats());
+    CHECK((const void*)&lc_root != (const void*)&lci_root);
+
+    lc a;
+    CHECK(lc_root.living == 1);
+    CHECK(lc_root.d_ctr == 1);
+
+    auto b = a;
+    CHECK(lc_root.c_ctr == 1);
+    CHECK(lc_root.copies == 1);
+    CHECK(lc_root.living == 2);
+
+    auto c = std::move(b);
+    CHECK(lc_root.m_ctr == 1);
+    CHECK(lc_root.copies == 1);
+    CHECK(lc_root.living == 3);
+
+    a = c;
+    CHECK(lc_root.c_asgn == 1);
+    CHECK(lc_root.copies == 2);
+
+    b = std::move(c);
+    CHECK(lc_root.m_asgn == 1);
+    CHECK(lc_root.living == 3);
+
+    {
+        lc::lifetime_stats local;
+        doctest::util::lifetime_counter_sentry sentry_local(local);
+        lc la;
+        CHECK(local.d_ctr == 1);
+        CHECK(local.living == 1);
+        {
+            lc::lifetime_stats sub_local;
+            doctest::util::lifetime_counter_sentry sentry_sub_local(sub_local);
+            auto slb = la;
+            CHECK(sub_local.c_ctr == 1);
+            CHECK(sub_local.copies == 1);
+            CHECK(sub_local.living == 1);
+            CHECK(local.living == 2);
+        }
+        CHECK(local.living == 1);
+    }
+
+    CHECK(lc_root.d_ctr == 2);
+    CHECK(lc_root.c_ctr == 2);
+    CHECK(lc_root.c_asgn == 1);
+    CHECK(lc_root.copies == 3);
+    CHECK(lc_root.m_ctr == 1);
+    CHECK(lc_root.m_asgn == 1);
+    CHECK(lc_root.living == 3);
+
+    lci::lifetime_stats empty_stats;
+    CHECK(lci_root == empty_stats);
 }
