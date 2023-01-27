c2bae0b70f7f935e846d2185ccc7f9532e075f01
Borislav Stanimirov <b.stanimirov@abv.bg>
2023-01-27 14:44:34 +0200

    atomic_relaxed_counter: copying, explicit ctor, explicit ops

--- code/doctest/util/atomic_relaxed_counter.hpp
+++ code/doctest/util/atomic_relaxed_counter.hpp
@@ -18,23 +18,28 @@ struct atomic_relaxed_counter {
     static const std::memory_order rel = std::memory_order_relaxed;
 
     atomic_relaxed_counter() noexcept = default;
-    atomic_relaxed_counter(I init) noexcept : a(init) {};
+    explicit atomic_relaxed_counter(I init) noexcept : a(init) {};
 
-    atomic_relaxed_counter(const atomic_relaxed_counter&) = delete;
-    atomic_relaxed_counter& operator=(const atomic_relaxed_counter&) = delete;
-
-    operator I() const noexcept {
-        return a.load(rel);
+    atomic_relaxed_counter(const atomic_relaxed_counter& b) : a(b.load()) {}
+    atomic_relaxed_counter& operator=(const atomic_relaxed_counter& b) {
+        a.store(b.load());
+        return *this;
     }
+
+    I load() const noexcept { return a.load(rel); }
+    void store(I i) noexcept { return a.store(i, rel); }
+
+    operator I() const noexcept { return load(); } // intentionally implicit
+
     I operator++() noexcept { return a.fetch_add(1, rel) + 1; }
     I operator++(int) noexcept { return a.fetch_add(1, rel); }
     I operator+=(I i) noexcept { return a.fetch_add(i, rel) + i; }
     I operator--() noexcept { return a.fetch_sub(1, rel) - 1; }
-    I operator-(int) noexcept { return a.fetch_sub(1, rel); }
+    I operator--(int) noexcept { return a.fetch_sub(1, rel); }
     I operator-=(I i) noexcept { return a.fetch_sub(i, rel) - i; }
 
     I operator=(I i) {
-        a.store(i, rel);
+        store(i);
         return i;
     }
 };
